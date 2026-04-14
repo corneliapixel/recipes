b9259e6ffc5f2a3ced3b36d594a39f58d88d1aa3
Cornelia Ärlerud <cornelia.arlerud@gmail.com>
2026-04-14 15:47:03 +0200

    feat: add show_menu function for menu display

--- recipe_selector.py
+++ recipe_selector.py
@@ -1,6 +1,26 @@
 import random
 
-recipes = ["Tacos", "Chili sin carne", "Bolognese", "Korvstroganoff"]
-recipe = (random.choice(recipes))
+def show_menu():    # show menu for user 
+    print("Welcome to the Randomized Recipe Generator!")
+    print("☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰")
+    print("  Any specific cravings?")
+    print("☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰")
+    print("  1. Vegetarian")
+    print("  2. Healthy option")
+    print("  3. Fastfood")
+    print("  4. Cheap option")
+    print("  5. Random, please!")
+    print("☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰")
 
-print(f"Idag blir det {recipe} till middag!")
+
+recipes = ["Tacos", "Chili sin carne", "Bolognese", "Korvstroganoff"]
+recipe_name = (random.choice(recipes))
+
+print(f"Idag blir det {recipe_name} till middag!")
+
+
+
+
+    
+
+
