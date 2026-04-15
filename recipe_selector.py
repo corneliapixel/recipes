cf24ab0960e171163e2b2c39e13088b9d352ae66
Cornelia Ärlerud <cornelia.arlerud@gmail.com>
2026-04-15 15:15:05 +0200

    feat: add recipe lists by menu category

--- recipe_selector.py
+++ recipe_selector.py
@@ -12,6 +12,13 @@ def show_menu():    # show menu for user
     print("  5. Random, please!")
     print("☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰")
 
+vegetarian_recipes = ["Vegetarian Curry with Cauliflower and Chickpeas", "Vegetable Stir-Fry", "Halloumi and Carrot Steaks", "Sweetpotato Soup with Chevre", "Enchiladas"]
+
+healthy_recipes = ["Salmon with Quinoa", "Grilled Chicken Salad", "Tuna Salad Asian Style", "Avocado Shrimp Salad", "Pumpkin Soup"]
+
+fastfood_recipes = ["Burgers and Fries", "Homemade Pizza", "Hot Dogs", "Mac 'n' Cheese", "Spaghetti and Meatballs"]
+
+cheap_recipes = ["Carrot Biryani", "Nasi Goreng", "Spaghetti Carbonara", "Lentil Soup", "What's in the Fridge?"]
 
 recipes = ["Tacos", "Chili sin carne", "Bolognese", "Korvstroganoff"]
 recipe_name = (random.choice(recipes))
