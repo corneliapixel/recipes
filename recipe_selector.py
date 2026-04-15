b6b640e8c808637c8b431c8b3fc205f2451ed9f9
Cornelia Ärlerud <cornelia.arlerud@gmail.com>
2026-04-15 19:30:39 +0200

    fix: correct menu selection and output

--- recipe_selector.py
+++ recipe_selector.py
@@ -6,9 +6,9 @@ def show_menu():    # show menu for user
     print("  Any specific cravings?")
     print("☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰")
     print("  1. Vegetarian")
-    print("  2. Healthy option")
-    print("  3. Fastfood")
-    print("  4. Cheap option")
+    print("  2. Healthy food")
+    print("  3. Fast food")
+    print("  4. Cheap food")
     print("  5. Random, please!")
     print("☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰")
 
@@ -20,36 +20,32 @@ fastfood_recipes = ["Burgers and Fries", "Homemade Pizza", "Hot Dogs", "Mac 'n' 
 
 cheap_recipes = ["Carrot Biryani", "Nasi Goreng", "Spaghetti Carbonara", "Lentil Soup", "What's in the Fridge?"]
 
-recipes = ["Tacos", "Chili sin carne", "Bolognese", "Korvstroganoff"]
-recipe_name = (random.choice(recipes))
-
-print(f"Idag blir det {recipe_name} till middag!")
-
 
 def main (): 
 
     while True:
         show_menu()
-        preference = input("Pick your potion: (1-5)").strip()
+        preference = input("Pick your potion: (1-5)\t").strip()
+        print("")
 
         if preference == '1':
-            preference = random.choice(vegetarian_recipes) 
+            recipe = random.choice(vegetarian_recipes) 
 
         elif preference == '2':
-            preference = random.choice(healthy_recipes)
+            recipe = random.choice(healthy_recipes)
 
         elif preference == '3':
-            preference = random.choice(fastfood_recipes)
+            recipe = random.choice(fastfood_recipes)
 
         elif preference == '4':
-            preference = random.choice(cheap_recipes) 
+            recipe = random.choice(cheap_recipes) 
 
         elif preference == '5':
             all_recipes = (vegetarian_recipes + 
                                healthy_recipes + 
                                fastfood_recipes + 
                                cheap_recipes)
-            preference = random.choice(all_recipes)
+            recipe = random.choice(all_recipes)
 
         else: 
             print("Sorry, I can't help you...\n")
@@ -57,7 +53,7 @@ def main ():
 
         print("Analyzing cravings...")
         print("Almost there...")
-        print(f"Tonight's dinner: {preference}")
+        print(f"Tonight's dinner: {recipe}\n")
 
 main() 
 
